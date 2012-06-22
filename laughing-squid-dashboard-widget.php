5679f33ebda95a720d59d9558da3d479e9249ac6
sdenike <sdenike@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-22 23:19:34 +0000

    Initial Commit
    git-svn-id: https://plugins.svn.wordpress.org/laughing-squid-dashboard-widget/tags/1.0@562739 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ laughing-squid-dashboard-widget.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Plugin Name: Laughing Squid Dashboard Widget
+Plugin URI: http://www.laughingsquid.us
+Description: The Laughing Squid Dashboard Widget will provide you with news from the status blog from within your WordPress Dashboard.
+Version: 1.0
+Author: Shelby DeNike
+Author URI: http://www.sd3labs.com
+*/
+
+
+//Function to display the RSS feed.
+function ls_rss_dashboard_widget_function() {
+	$rss = fetch_feed( "http://laughingsquid.wordpress.com/feed/" );
+
+	if ( is_wp_error($rss) ) {
+		if ( is_admin() || current_user_can('manage_options') ) {
+			echo '<p>';
+			printf(__('<strong>RSS Error</strong>: %s'), $rss->get_error_message());
+			echo '</p>';
+		}
+		return;
+	}
+
+	if ( !$rss->get_item_quantity() ) {
+		echo '<p>There is currently no news available.</p>';
+		$rss->__destruct();
+		unset($rss);
+		return;
+	}
+
+	echo "<ul>\n";
+
+	if ( !isset($items) )
+		$items = 5;
+
+	foreach ( $rss->get_items(0, $items) as $item ) {
+		$publisher = '';
+		$site_link = '';
+		$link = '';
+		$content = '';
+		$date = $item->get_date('F j, Y');
+		$link = esc_url( strip_tags( $item->get_link() ) );
+		$rss_title = $item->get_title();
+
+		$content = $item->get_content();
+		$content = wp_html_excerpt($content, 250) . ' ...';
+
+		echo "\t<li><a href='$link'>$rss_title</a> - ($date)<br>$content</li><hr>\n";
+	}
+
+	echo "</ul>\n";
+	$rss->__destruct();
+	unset($rss);
+};
+
+//Function to add the rss feed to the dashboard.
+function ls_rss_add_dashboard_widget() {
+	wp_add_dashboard_widget('ls_rss_dashboard_widget', 'Laughing Squid News', 'ls_rss_dashboard_widget_function');
+}
+
+//Action that calls the function that adds the widget to the dashboard.
+add_action('wp_dashboard_setup', 'ls_rss_add_dashboard_widget');
+?>
---
+++ readme.txt
@@ -0,0 +1,25 @@
+=== Laughing Squid Dashboard Widget ===
+Contributors: sdenike
+Donate link: http://sd3labs.com/
+Tags: news, hosting, dashboard
+Requires at least: 3.1
+Tested up to: 3.4
+Stable tag: trunk
+
+The Laughing Squid Dashboard Widget will provide you with news from the status blog from within your WordPress Dashboard.
+
+== Description ==
+
+The Laughing Squid Dashboard Widget will provide you with news from the status blog from within your WordPress Dashboard.
+
+== Installation ==
+
+1. Upload `laughing-squid-dashboard-widget.php` to the `wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Enjoy!, there are no settings for this.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release.
+
